e1221b387299028852c82c688a2bdad9d180ea3a
Nick Schafer <133047743+Nmsviper93@users.noreply.github.com>
2024-08-22 12:19:11 -0400

    Update axiosInstance.js

--- Frontend/src/Services/axiosInstance.js
+++ Frontend/src/Services/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-import { jwtDecode } from 'jwt-decode';
-import userService from './userService';
+// import { jwtDecode } from 'jwt-decode';
+// import userService from './userService';
 
 const axiosInstance = axios.create({ 
     baseURL: 'https://api.spoonacular.com',
@@ -10,27 +10,27 @@ const axiosInstance = axios.create({
 });
 
 // interceptor to add JWT token to each request
-axiosInstance.interceptors.request.use(async (config) => {
-    let token = localStorage.getItem('jwtToken');
-    if (token) {
-        const { exp } = jwtDecode(token);
-        if (Date.now() >= exp * 1000) {
-            try {
-                const response = await axios.post('/api/users/refresh-token', { token });
-                token = response.data.token;
-                localStorage.setItem('jwtToken', token);
-            } catch (error) {
-                userService.logout();
-                // redirect to login on failure to refresh token
-                window.location.href = '/login';
-                throw error;
-            }
-        }
-        config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+// axiosInstance.interceptors.request.use(async (config) => {
+//     let token = localStorage.getItem('jwtToken');
+//     if (token) {
+//         const { exp } = jwtDecode(token);
+//         if (Date.now() >= exp * 1000) {
+//             try {
+//                 const response = await axios.post('/api/users/refresh-token', { token });
+//                 token = response.data.token;
+//                 localStorage.setItem('jwtToken', token);
+//             } catch (error) {
+//                 userService.logout();
+//                 // redirect to login on failure to refresh token
+//                 window.location.href = '/login';
+//                 throw error;
+//             }
+//         }
+//         config.headers['Authorization'] = `Bearer ${token}`;
+//     }
+//     return config;
+// }, (error) => {
+//     return Promise.reject(error);
+// });
 
 export default axiosInstance;
